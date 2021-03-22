8f0450fae79722336b60b59aac147b0b454294c3
josedoce <josesamya@gmail.com>
2021-03-22 19:56:05 -0300

    interfaces

--- src/tipagem/index.ts
+++ src/tipagem/index.ts
@@ -3,5 +3,12 @@ interface CreateProduct {
     description: string;
     unid_disp: number;
     price: number;
+    peso:string;
+    formato:string;
+    comprimento:string;
+    altura:string;
+    largura:string;
+    servico:string;
+    diametro:string;
 }
 export{CreateProduct};
